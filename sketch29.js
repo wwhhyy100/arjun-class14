90b96c6724cb8bc049419681076862198f506f1b
wwhhyy100 <87866392+wwhhyy100@users.noreply.github.com>
2021-07-30 11:06:57 -0400

    Add files via upload

--- sketch29.js
+++ sketch29.js
@@ -1,36 +1,43 @@
 var ship,ship_sailing;
-var sea
+var sea,seaImage;
 
 function preload(){
+
   seaImage = loadImage("sea.png");
   
   ship_sailing = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
 
-  sea.addImage(seaImage);
 
 }
 
 function setup(){
   createCanvas(400,400);
   //creating the ship
-  ship = createSprite(200,200);
-  ship.addAnimation("sailing",ship_sailing);
+
 
   //making the sea background
-  sea=createSprite(200,400,400,20);
-  sea.addImage(sea.png)
+  sea=createSprite(400,200,20,20);
+  sea.addImage(seaImage);
+  sea.velocityX=-5
+  sea.scale = 0.3
+
+  //creating the ship
+  ship = createSprite(130,260,20,20);
+  ship.addAnimation("sailing",ship_sailing);
+  ship.scale = 0.2
+
 
 }
 
+
+
 function draw() {
-  background(sea.png);
+  background("blue");
 
-
-
- sea.velocityX=5
   //code to make backgrond infenite 
 
-  if(sea.x=0){
-  sea.x=sea.width/2
+  if(sea.x < 0){
+  sea.x=sea.width/30
   }
+  drawSprites()
 }
